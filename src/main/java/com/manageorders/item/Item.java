9356005d82bfc8bd6efdecb653063a3f2138d110
Renan Portugal do Rio <55991676+RenanRPDR@users.noreply.github.com>
2023-06-21 18:33:03 +0100

    chore: Hibernate problem to create table solved

---
+++ src/main/java/com/manageorders/item/Item.java
@@ -0,0 +1,59 @@
+package com.manageorders.item;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "items")
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Item(Long id, String name) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public Item() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return Objects.equals(id, item.id) && Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
