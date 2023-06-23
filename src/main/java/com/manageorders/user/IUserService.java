4e794013bf1b12712a2fefeab62207577cad4304
Renan Portugal do Rio <55991676+RenanRPDR@users.noreply.github.com>
2023-06-23 18:49:23 +0100

    feat: User CRUD created.

---
+++ src/main/java/com/manageorders/user/IUserService.java
@@ -0,0 +1,14 @@
+package com.manageorders.user;
+
+import com.manageorders.user.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IUserService {
+    User createUser(User user);
+    Optional<User> getUserById(Long id);
+    List<User> getAllUsers();
+    User updateUser(Long id, User user);
+    void deleteUser(Long id);
+}
---
+++ src/main/java/com/manageorders/user/UserController.java
@@ -0,0 +1,59 @@
+package com.manageorders.user;
+
+import com.manageorders.item.Item;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        Optional<User> item = userService.getUserById(id);
+        return item.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllItems() {
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        Optional<User> existingUser = userService.getUserById(id);
+        if (existingUser.isPresent()) {
+            user.setId(id);
+            User updatedUser = userService.updateUser(id, user);
+            return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        Optional<User> user = userService.getUserById(id);
+        if (user.isPresent()) {
+            userService.deleteUser(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
